f1a347d4614b9ad33c3055be31ebdecc7fcc38a3
David Ulrich <david@DavidsWebserver.local>
2016-07-08 07:24:02 +0200

    no message

---
+++ Davids_Config/startFocuschainData.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#geth --datadir focuschainData --networkid 13041969 --port 30301 --rpcport 8101 console
+#geth --genesis focuschain-genesis.json --datadir focuschainData --networkid 13041969 --port 30301 --rpcport 8101 --ipcpath focuschainData --dev console
+#geth --genesis focuschain-genesis.json --datadir focuschainData --networkid 19700105 --port 30301 --rpcport 8101 --ipcpath /Users/david/Library/Ethereum/geth.ipc  --verbosity 6 --preload functions.js console 2>> eth.log
+geth --genesis focuschain-genesis.json --datadir focuschainData --networkid 19700105 --port 30301 --rpcapi "eth,net,web3" --rpcport 8101 --rpc --rpcaddr 192.168.2.111 --ipcpath /Users/david/Library/Ethereum/geth.ipc  --identity david --nodiscover --verbosity 5 --preload functions.js console 2>> eth.log
